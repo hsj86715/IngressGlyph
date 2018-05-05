cb923e3daf2f08c5172c6e989e5ef86197ba8ac8
hsj86715 <hsj86715@gmail.com>
2018-05-05 10:13:18 +0800

    improve UI and function update to version 1.1.0 90%
    Signed-off-by: shujun.hu.x <shujun.hu.x@cloudminds.com>

--- glyph/src/main/java/com/hsj86715/ingress/glyph/AboutActivity.java
+++ glyph/src/main/java/com/hsj86715/ingress/glyph/AboutActivity.java
@@ -1,14 +1,18 @@
 package com.hsj86715.ingress.glyph;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 /**
  * @author hushujun
  */
 public class AboutActivity extends AppCompatActivity {
+    private TextView mVersionTx;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +28,19 @@ public class AboutActivity extends AppCompatActivity {
                 finish();
             }
         });
+        mVersionTx = findViewById(R.id.about_version);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        try {
+            PackageManager manager = this.getPackageManager();
+            PackageInfo info = manager.getPackageInfo(this.getPackageName(), 0);
+            String versionName = info.versionName;
+            mVersionTx.setText("v" + versionName);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
